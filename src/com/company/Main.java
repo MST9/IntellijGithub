19220876f5ebb0cde91da2cd32c09f33556a0810
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-12-13 11:53:25 +0530

    Created another branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Let's hope I am able to run git and github from intelliJ terminal.");
+        System.out.println("Creating different branch");
 
     }
 }
