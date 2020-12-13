b078c887a46f3c464d4d7e65b3a9d61d297d6c65
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-12-13 13:47:50 +0530

    One more time conflict!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Let's hope I am able to run git and github from intelliJ terminal. Trying to create conflict let's work!");
+        System.out.println("Let's hope I am able to run git and github from intelliJ terminal. Trying to create conflict let's work anna!");
     }
 }
