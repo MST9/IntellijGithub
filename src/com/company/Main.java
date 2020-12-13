4e40ec77036db12cd7baaf9697c4adeb9877b53b
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-12-13 11:43:35 +0530

    Second attempt to run git and github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Let's hope I am able to run git and github from intelliJ terminal.");
+
     }
 }
