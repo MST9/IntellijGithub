06036587c4805d1863e683f47c1a127910a1ca5a
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-12-13 14:11:44 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Let's hope I am able to run git and github from intelliJ terminal. Trying to create conflict let's work anna as a team!");
+        System.out.println("Let's hope I am able to run git and github from intelliJ terminal. Trying to create conflict let's work anna as a team!. Hope I learn git stash!");
     }
 }
