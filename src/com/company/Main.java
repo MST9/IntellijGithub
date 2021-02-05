36c16edcef7da52404a624f2800812b4d863f92f
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2021-02-05 17:51:04 +0530

    Created new branch king in local and added it to Github from terminal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Let's hope I am able to run git and github from intelliJ terminal. Trying to create conflict let's work anna as a team!. Work with git stash anna.");
+        System.out.println("Created new branch locally and added it to github from terminal");
+
     }
 }
